2f30a428a3b19e080c7fa9f221deaebb6fe74ecc
petterobam <1460300366@qq.com>
2018-02-25 18:56:32 +0800

    修改为个人的信息

--- static/js/script.js
+++ static/js/script.js
@@ -12,7 +12,7 @@ if (!String.prototype.format) {
 }
 
 
-jQuery(document).ready(function($) {
+/*jQuery(document).ready(function($) {
     $(".left-label").attr('contenteditable', 'true');
     $(".label-value").attr('contenteditable', 'true');
     $(".info-title").attr('contenteditable', 'true');
@@ -46,22 +46,22 @@ jQuery(document).ready(function($) {
     });
 
     $('.info-header').hover(function() {
-        /* Stuff to do when the mouse enters the element */
+        // Stuff to do when the mouse enters the element
         $(this).children('.unit-remove').css('visibility', 'visible');
         $(this).children('.item-add').css('visibility', 'visible');
 
     }, function() {
-        /* Stuff to do when the mouse leaves the element */
+        // Stuff to do when the mouse leaves the element 
         $(this).children('.unit-remove').css('visibility', 'hidden');
         $(this).children('.item-add').css('visibility', 'hidden');
     });
 
     $('.info-unit ul li').hover(function() {
-        /* Stuff to do when the mouse enters the element */
+        // Stuff to do when the mouse enters the element 
         $(this).children('.item-remove').css('visibility', 'visible');
 
     }, function() {
-        /* Stuff to do when the mouse leaves the element */
+        // Stuff to do when the mouse leaves the element
         $(this).children('.item-remove').css('visibility', 'hidden');
     });
 
@@ -87,4 +87,4 @@ jQuery(document).ready(function($) {
     });
     // $("*").removeAttr('contenteditable');
 
-});
+});*/
